23cab28cbf65c9718c454915b3240662292d9ffd
Zekarias Araya <zekarias99@gmail.com>
2013-01-09 06:06:42 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '359cc5017889851d044df8aec212b73190c4a8446d01672a0a019150b21b661b3472783655117896b67ceccd15191fb42dd89014bbbd8f42952d787e48fde919'
